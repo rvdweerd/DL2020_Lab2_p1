65f9e177e8329eef443dfc8b3fa09cf182c2baac
rvdw_vbook <rogerscode@gmail.com>
2020-11-18 08:10:58 +0100

    test from vbook

--- test.py
+++ test.py
@@ -10,6 +10,6 @@ data_loader = torch.utils.data.DataLoader(bp_dataset, batch_size=3)
 data_inputs, data_labels = next(iter(data_loader))
 print("Data inputs", data_inputs.shape, "\n", data_inputs)
 #print("Data labels", data_labels.shape, "\n", data_labels)
-
+# added comment local laptop
 
 
