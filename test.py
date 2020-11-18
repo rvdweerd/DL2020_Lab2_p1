ba7083d2f76095672e2028e6ef5f580ed0a0ca05
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-11-18 08:02:58 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+from datasets import BinaryPalindromeDataset
+
+import numpy as np
+import torch
+#import torch.utils.data as data
+
+
+bp_dataset = BinaryPalindromeDataset(2)
+data_loader = torch.utils.data.DataLoader(bp_dataset, batch_size=3)
+data_inputs, data_labels = next(iter(data_loader))
+print("Data inputs", data_inputs.shape, "\n", data_inputs)
+#print("Data labels", data_labels.shape, "\n", data_labels)
+
+
+
