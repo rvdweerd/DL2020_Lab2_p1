01ee621ecc6a12736c4abaa881848ff8053472ca
RogerVDW_VS2019 <rogerscode@gmail.com>
2020-11-18 21:46:06 +0100

    lstm impl with backprop error

--- lstm.py
+++ lstm.py
@@ -7,6 +7,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
 import torch.nn as nn
 
 
@@ -16,10 +17,44 @@ class LSTM(nn.Module):
                  batch_size, device):
 
         super(LSTM, self).__init__()
+
+
         ########################
         # PUT YOUR CODE HERE  #
         #######################
-        raise NotImplementedError
+        self.seq_length = seq_length
+        self.batch_size = batch_size
+        self.input_dim = input_dim
+        self.C=torch.zeros(hidden_dim)
+        self.h=torch.zeros(hidden_dim)
+
+        self.Wfx = nn.Parameter(torch.ones(input_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wfx, mode='fan_out', nonlinearity='sigmoid')
+        self.Wfh = nn.Parameter(torch.ones(hidden_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wfh, mode='fan_out', nonlinearity='sigmoid')
+        self.bf = nn.Parameter(torch.zeros(hidden_dim),requires_grad=True)
+
+        self.Wix = nn.Parameter(torch.ones(input_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wix, mode='fan_out', nonlinearity='sigmoid')
+        self.Wih = nn.Parameter(torch.ones(hidden_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wih, mode='fan_out', nonlinearity='sigmoid')
+        self.bi = nn.Parameter(torch.zeros(hidden_dim),requires_grad=True)
+
+        self.Wgx = nn.Parameter(torch.ones(input_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wgx, mode='fan_out', nonlinearity='sigmoid')
+        self.Wgh = nn.Parameter(torch.ones(hidden_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wgh, mode='fan_out', nonlinearity='sigmoid')
+        self.bg = nn.Parameter(torch.zeros(hidden_dim),requires_grad=True)
+
+        self.Wox = nn.Parameter(torch.ones(input_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wox, mode='fan_out', nonlinearity='sigmoid')
+        self.Woh = nn.Parameter(torch.ones(hidden_dim,hidden_dim),requires_grad=True)
+        nn.init.kaiming_normal_(self.Woh, mode='fan_out', nonlinearity='sigmoid')
+        self.bo = nn.Parameter(torch.zeros(hidden_dim),requires_grad=True)
+
+        self.Wph = nn.Parameter(torch.ones(hidden_dim,num_classes),requires_grad=True)
+        nn.init.kaiming_normal_(self.Wph, mode='fan_out', nonlinearity='sigmoid')
+        self.bp = nn.Parameter(torch.zeros(num_classes),requires_grad=True)
         ########################
         # END OF YOUR CODE    #
         #######################
@@ -28,7 +63,18 @@ class LSTM(nn.Module):
         ########################
         # PUT YOUR CODE HERE  #
         #######################
-        raise NotImplementedError
+        for s in range(self.batch_size):
+            for t in range(self.seq_length):
+                sig = torch.nn.Sigmoid()
+                tanh = torch.nn.Tanh()
+                f=sig(torch.matmul(x[s,t,:],self.Wfx) + torch.matmul(self.h,self.Wfh) + self.bf)
+                i=sig(torch.matmul(x[s,t,:],self.Wix) + torch.matmul(self.h,self.Wih) + self.bi)
+                g=sig(torch.matmul(x[s,t,:],self.Wgx) + torch.matmul(self.h,self.Wgh) + self.bg)
+                o=sig(torch.matmul(x[s,t,:],self.Wox) + torch.matmul(self.h,self.Woh) + self.bo)
+                self.C = g*i + self.C*f
+                self.h = o*tanh(self.C)
+        y=torch.matmul(self.h,self.Wph)+self.bp
+        return y
         ########################
         # END OF YOUR CODE    #
         #######################
--- test.py
+++ test.py
@@ -2,15 +2,44 @@ from datasets import BinaryPalindromeDataset
 
 import numpy as np
 import torch
+from lstm import LSTM
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
 #import torch.utils.data as data
+N_PALIN = 2
+seq_len = N_PALIN*4+1
+BATCH_SIZE = 1
+N_CLASSES=1 # just binary classifier
+INPUT_DIM=10
+HIDDEN_DIM=15
 
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    # added seed for GPU
+    torch.cuda.manual_seed(42)
+    torch.cuda.manual_seed_all(42)
+else:
+    device = torch.device('cpu')
+print('device used:',device)
 
+embedding = torch.nn.Embedding(N_CLASSES+2,INPUT_DIM,padding_idx=0)
 bp_dataset = BinaryPalindromeDataset(2)
-data_loader = torch.utils.data.DataLoader(bp_dataset, batch_size=3)
-data_inputs, data_labels = next(iter(data_loader))
-print("Data inputs", data_inputs.shape, "\n", data_inputs)
-#print("Data labels", data_labels.shape, "\n", data_labels)
-# added comment local laptop
-# comment added on dekstop
+data_loader = torch.utils.data.DataLoader(bp_dataset, batch_size=BATCH_SIZE)
 
-
+lstm = LSTM(seq_len,INPUT_DIM,HIDDEN_DIM,N_CLASSES,BATCH_SIZE,device)
+loss_module = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(lstm.parameters(),lr=1e-3)
+print('Number of trainable parameters: ',count_parameters(lstm))
+torch.autograd.set_detect_anomaly(True)
+for i in range(100):
+    data_inputs, data_labels = next(iter(data_loader))
+    x_emb=embedding((data_inputs.squeeze(2)).type(torch.LongTensor))
+    pred = lstm(x_emb)
+    loss=loss_module(pred,data_labels.float())
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print('step ',i,'loss=',loss)
