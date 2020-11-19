8a671f78e4e03f7e5f205a23a70282d5335c8698
rvdw_vbook <rogerscode@gmail.com>
2020-11-19 20:01:12 +0100

    hyperparam scan defined

--- lstm.py
+++ lstm.py
@@ -75,6 +75,7 @@ class LSTM(nn.Module):
         self.C = torch.zeros(self.batch_size,self.hidden_dim).to(self.device)
         self.h=torch.zeros(self.batch_size,self.hidden_dim).to(self.device)
         for t in range(self.seq_length):
+            #resetMatrix = torch.diag((x_in[:,t]>0).type(torch.FloatTensor)).to(self.device)
             sig = torch.nn.Sigmoid()
             tanh = torch.nn.Tanh()
             f=sig(torch.matmul(x[:,t,:],self.Wfx) + torch.matmul(self.h,self.Wfh) + self.bf)
@@ -82,7 +83,7 @@ class LSTM(nn.Module):
             g=sig(torch.matmul(x[:,t,:],self.Wgx) + torch.matmul(self.h,self.Wgh) + self.bg)
             o=sig(torch.matmul(x[:,t,:],self.Wox) + torch.matmul(self.h,self.Woh) + self.bo)
             self.C = g*i + self.C*f
-            self.C = torch.diag((x_in[:,t]>0).type(torch.FloatTensor)) @self.C # Reset state vector to zero if sequence hasn't started yet (padding symbol)
+            #self.C = resetMatrix @self.C # Reset state vector to zero if sequence hasn't started yet (padding symbol)
             self.h = o*tanh(self.C)
         y=torch.matmul(self.h,self.Wph)+self.bp
         return y.squeeze()
